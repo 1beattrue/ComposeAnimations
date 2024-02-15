50bda9e1e9bc5d0503d132ce4e77c44390186ae8
Vitaly <onebeattrue@gmail.com>
2024-02-15 19:40:06 +0300

    Анимация resize (animateDpAsState)

--- app/src/main/java/edu/mirea/onebeattrue/composeanimations/Test.kt
+++ app/src/main/java/edu/mirea/onebeattrue/composeanimations/Test.kt
@@ -1,5 +1,6 @@
 package edu.mirea.onebeattrue.composeanimations
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,12 +14,20 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun Test() {
     Column(
@@ -30,17 +39,24 @@ fun Test() {
             ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var isIncreased by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val size = animateDpAsState(targetValue = if (isIncreased) 300.dp else 200.dp)
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = { isIncreased = !isIncreased }
         ) {
             Text(
                 text = "Animate size",
             )
         }
         AnimatedContainer(
-            text = "Size"
+            text = "Size",
+            sizeState = size
         )
+
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {}
@@ -52,6 +68,8 @@ fun Test() {
         AnimatedContainer(
             text = "Shape"
         )
+
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {}
@@ -63,6 +81,8 @@ fun Test() {
         AnimatedContainer(
             text = "Border"
         )
+
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {}
@@ -74,6 +94,8 @@ fun Test() {
         AnimatedContainer(
             text = "Color"
         )
+
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {}
@@ -90,13 +112,14 @@ fun Test() {
 
 @Composable
 private fun AnimatedContainer(
-    text: String
+    text: String,
+    sizeState: State<Dp> = mutableStateOf(200.dp)
 ) {
     Box(
         modifier = Modifier
             .clip(RoundedCornerShape(8.dp))
             .background(Color.Blue)
-            .size(200.dp),
+            .size(sizeState.value),
         contentAlignment = Alignment.Center
     ) {
         Text(
