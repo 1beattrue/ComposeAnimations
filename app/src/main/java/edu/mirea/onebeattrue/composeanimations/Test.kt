4ecf78510b801c5e23b49bfcf9e789882d1128ad
Vitaly <onebeattrue@gmail.com>
2024-02-15 20:11:50 +0300

    Анимация border (animateDpAsState); Анимация color (animateColorAsState); Анимация visibility (animateFloatAsState);

--- app/src/main/java/edu/mirea/onebeattrue/composeanimations/Test.kt
+++ app/src/main/java/edu/mirea/onebeattrue/composeanimations/Test.kt
@@ -1,8 +1,11 @@
 package edu.mirea.onebeattrue.composeanimations
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,6 +25,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -74,43 +78,55 @@ fun Test() {
             cornerRadiusPercentState = cornerRadius
         )
 
-
+        var isBordered by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val borderWidth = animateDpAsState(targetValue = if (isBordered) 50.dp else 0.dp)
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = { isBordered = !isBordered }
         ) {
             Text(
                 text = "Animate border",
             )
         }
         AnimatedContainer(
-            text = "Border"
+            text = "Border",
+            borderWidthState = borderWidth
         )
 
-
+        var isRed by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val color = animateColorAsState(targetValue = if (isRed) Color.Magenta else Color.Blue)
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = { isRed = !isRed }
         ) {
             Text(
                 text = "Animate color",
             )
         }
         AnimatedContainer(
-            text = "Color"
+            text = "Color",
+            colorState = color
         )
 
-
+        var isTransparent by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val alpha = animateFloatAsState(targetValue = if (isTransparent) 0f else 1f)
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = { isTransparent = !isTransparent }
         ) {
             Text(
                 text = "Animate visibility",
             )
         }
         AnimatedContainer(
-            text = "Visibility"
+            text = "Visibility",
+            alphaState = alpha
         )
     }
 }
@@ -119,13 +135,18 @@ fun Test() {
 private fun AnimatedContainer(
     text: String,
     sizeState: State<Dp> = mutableStateOf(200.dp),
-    cornerRadiusPercentState: State<Int> = mutableStateOf(4)
+    cornerRadiusPercentState: State<Int> = mutableStateOf(4),
+    borderWidthState: State<Dp> = mutableStateOf(0.dp),
+    colorState: State<Color> = mutableStateOf(Color.Blue),
+    alphaState: State<Float> = mutableStateOf(1f)
 ) {
     Box(
         modifier = Modifier
+            .alpha(alphaState.value)
             .clip(RoundedCornerShape(cornerRadiusPercentState.value))
-            .background(Color.Blue)
-            .size(sizeState.value),
+            .background(colorState.value)
+            .size(sizeState.value)
+            .border(width = borderWidthState.value, color = Color.Black),
         contentAlignment = Alignment.Center
     ) {
         Text(
